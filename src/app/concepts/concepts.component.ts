41d67cf0f189d97193fbf4c38d5060c4351dcd66
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-10-20 18:02:45 +0530

    day 2

--- src/app/concepts/concepts.component.ts
+++ src/app/concepts/concepts.component.ts
@@ -4,13 +4,42 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-concepts',
   templateUrl: './concepts.component.html',
   styles: [
+    `
+      .redText{
+        color: red;
+      }
+
+      .greenText{
+        color: green;
+      }
+    `
   ]
 })
 export class ConceptsComponent implements OnInit {
 
-  constructor() { }
+  // public variables, private variable
+
+  // String Interpolation
+  appName = 'User Management App!';
+  year = 2020;
+
+  // property binding
+  companyName = 'CGI';
+  isLoggedIn = false;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  // ideal place for your custom methods
+  clickMeHandler( event ){ // event - an object
+    console.log(event);
+    // todo: change the button text as clicked
+    // todo: disable the button also
+    alert('clicked');
   }
 
 }
